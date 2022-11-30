71476bfcc801723e9f54816849a3198dca7b4411
Sangmin Kim <poby@Pobys-MacBook-Air.local>
2022-11-30 18:03:48 +0900

    221130 create member_tbl and board_tbl for sample internet cafe

--- SQL/scott_11_28_member.sql
+++ SQL/scott_11_28_member.sql
@@ -2,3 +2,6 @@ INSERT INTO member_tbl(username, password, name, tel)
 VALUES ('smg1024', 'dork!024', 'Sangmin Kim', '010-1234-5678');
 
 SELECT * FROM member_tbl;
+
+SELECT * FROM user_constraints;
+purge recyclebin;
