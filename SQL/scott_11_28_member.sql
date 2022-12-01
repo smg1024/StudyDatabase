1d2bcececc2a8f402ef0db298cf9335c8718ee7d
Sangmin Kim <poby@Pobys-MacBook-Air.local>
2022-12-02 01:38:28 +0900

    221201 modeling for board

--- SQL/scott_11_28_member.sql
+++ SQL/scott_11_28_member.sql
@@ -1,7 +1,25 @@
 INSERT INTO member_tbl(username, password, name, tel)
 VALUES ('smg1024', 'dork!024', 'Sangmin Kim', '010-1234-5678');
 
-SELECT * FROM member_tbl;
+INSERT INTO member_tbl(username, password, name, tel)
+VALUES ('poby', '981024', 'Poby', '010-9876-5432');
 
-SELECT * FROM user_constraints;
-purge recyclebin;
+CREATE SEQUENCE board_seq
+START WITH 1
+INCREMENT BY 1;
+
+INSERT INTO board_tbl(postno, subject, content, username, ipAddr)
+VALUES (board_seq.nextval,?,?,?,?);
+
+DROP SEQUENCE board_seq;
+
+SELECT postno, subject, username, hitcount, to_char(regdate, 'mm-dd HH:MI')
+FROM board_tbl ORDER BY postno DESC;
+
+UPDATE board_tbl SET subject=?, content=? WHERE postno=?;
+
+
+SELECT * FROM user_sequences;
+SELECT * FROM member_tbl;
+SELECT * FROM board_tbl;
+SELECT * FROM user_constraints WHERE table_name='BOARD_TBL';
